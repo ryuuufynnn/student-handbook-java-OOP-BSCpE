1659b7654e462b88464069bd7ab89b32aa029bfc
ryu.dev <comp.sci.lau@programmer.net>
2026-02-16 15:35:11 +0800

    activity 1, OOP student handbook

---
+++ activityOne.java
@@ -0,0 +1,13 @@
+public class activityOne {
+    public static void main(String[] args) {
+        // output
+        System.out.println("~ ! @ # $ % ^ & * ( ) _ + |");
+        System.out.println("' 1 2 3 4 5 6 7 8 9 0 - = \\");
+        System.out.println("q w e r t y u i o p [ ] { }");
+        System.out.println("a s d f g h j k l ; ` :");
+        System.out.println("z x c v b n m , . / < > ?");
+        
+    }
+}
+// problem 
+// create a flow chart and java porgram to display all the characters in the keyboard. 
