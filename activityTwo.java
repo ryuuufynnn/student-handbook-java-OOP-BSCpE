359ee277a29259f76f39e8ef2e74fcf82aa6c95e
ryu.dev <comp.sci.lau@programmer.net>
2026-02-16 15:52:56 +0800

    activity 1, OOP student handbook

---
+++ activityTwo.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class activityTwo {
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+        double numOne;
+        double numTwo;
+        double product;
+
+        System.out.print("Enter numOne: ");
+        numOne = inp.nextDouble();
+        System.out.print("Enter numTwo: ");
+        numTwo = inp.nextDouble();
+
+        product = numOne * numTwo;
+
+        System.out.println("String you entered is: " + product);
+
+        inp.close();
+    }
+}
